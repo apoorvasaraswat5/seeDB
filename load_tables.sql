6b1e95d0260deaa10013e9f34ab243e3e699898c
Apoorva Saraswat <100483287+apoorvasaraswat5@users.noreply.github.com>
2022-09-28 18:22:20 -0400

    Adding project files

---
+++ load_tables.sql
@@ -0,0 +1,22 @@
+create table split1 (age real, workclass text, fnlwgt real, education text, education_num real, marital_status text, occupation text, relationship text, race text, sex text, capital_gain real, capital_loss real, hours_per_week real, native_country text, economic_indicator text);
+create table split2 (age real, workclass text, fnlwgt real, education text, education_num real, marital_status text, occupation text, relationship text, race text, sex text, capital_gain real, capital_loss real, hours_per_week real, native_country text, economic_indicator text);
+create table split3 (age real, workclass text, fnlwgt real, education text, education_num real, marital_status text, occupation text, relationship text, race text, sex text, capital_gain real, capital_loss real, hours_per_week real, native_country text, economic_indicator text);
+create table split4 (age real, workclass text, fnlwgt real, education text, education_num real, marital_status text, occupation text, relationship text, race text, sex text, capital_gain real, capital_loss real, hours_per_week real, native_country text, economic_indicator text);
+create table split5 (age real, workclass text, fnlwgt real, education text, education_num real, marital_status text, occupation text, relationship text, race text, sex text, capital_gain real, capital_loss real, hours_per_week real, native_country text, economic_indicator text);
+create table split6 (age real, workclass text, fnlwgt real, education text, education_num real, marital_status text, occupation text, relationship text, race text, sex text, capital_gain real, capital_loss real, hours_per_week real, native_country text, economic_indicator text);
+create table split7 (age real, workclass text, fnlwgt real, education text, education_num real, marital_status text, occupation text, relationship text, race text, sex text, capital_gain real, capital_loss real, hours_per_week real, native_country text, economic_indicator text);
+create table split8 (age real, workclass text, fnlwgt real, education text, education_num real, marital_status text, occupation text, relationship text, race text, sex text, capital_gain real, capital_loss real, hours_per_week real, native_country text, economic_indicator text);
+create table split9 (age real, workclass text, fnlwgt real, education text, education_num real, marital_status text, occupation text, relationship text, race text, sex text, capital_gain real, capital_loss real, hours_per_week real, native_country text, economic_indicator text);
+create table split10 (age real, workclass text, fnlwgt real, education text, education_num real, marital_status text, occupation text, relationship text, race text, sex text, capital_gain real, capital_loss real, hours_per_week real, native_country text, economic_indicator text);
+\copy split1 from C:\Users\Public\CS645\see_db_dataset\splits\split_1.csv delimiter ',' csv;
+\copy split2 from C:\Users\Public\CS645\see_db_dataset\splits\split_2.csv delimiter ',' csv;
+\copy split3 from C:\Users\Public\CS645\see_db_dataset\splits\split_3.csv delimiter ',' csv;
+\copy split4 from C:\Users\Public\CS645\see_db_dataset\splits\split_4.csv delimiter ',' csv;
+\copy split5 from C:\Users\Public\CS645\see_db_dataset\splits\split_5.csv delimiter ',' csv;
+\copy split6 from C:\Users\Public\CS645\see_db_dataset\splits\split_6.csv delimiter ',' csv;
+\copy split7 from C:\Users\Public\CS645\see_db_dataset\splits\split_7.csv delimiter ',' csv;
+\copy split8 from C:\Users\Public\CS645\see_db_dataset\splits\split_8.csv delimiter ',' csv;
+\copy split9 from C:\Users\Public\CS645\see_db_dataset\splits\split_9.csv delimiter ',' csv;
+\copy split10 from C:\Users\Public\CS645\see_db_dataset\splits\split_10.csv delimiter ',' csv;
+
+create view census as select * from split1 union select * from split2 union select * from split3 union select * from split4 union select * from split5 union select * from split6 union select * from split7 union select * from split8 union select * from split9 union select * from split10;
